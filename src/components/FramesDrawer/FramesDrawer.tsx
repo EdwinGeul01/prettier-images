c7199567db0d232c419a320bd30bf92631466669
EdwinGeul01 <edwingeul01@gmail.com>
2025-08-04 23:49:58 -0600

    feat:  add image toolkit with zoom and pan functionality

--- src/components/FramesDrawer/FramesDrawer.tsx
+++ src/components/FramesDrawer/FramesDrawer.tsx
@@ -1,28 +1,38 @@
-import { Button, CloseButton, Drawer, Portal } from '@chakra-ui/react'
-import { FrameCard } from './components/FrameCard'
-import { useMainContext } from '../../context/useMainContext'
+import {
+  Button,
+  CloseButton,
+  Drawer,
+  Portal,
+  SegmentGroup,
+  Text,
+} from "@chakra-ui/react";
+import { FrameCard } from "./components/FrameCard";
+import { useMainContext } from "../../context/useMainContext";
 
 export const FramesDrawer = () => {
-  const {useFramesDrawer} = useMainContext()
+  const { useFramesDrawer } = useMainContext();
   const { isOpen, setIsOpen } = useFramesDrawer;
 
   return (
-      <Drawer.Root size={"lg"}  open={isOpen} >
+    <Drawer.Root size={"lg"} open={isOpen}>
       <Portal>
         <Drawer.Backdrop />
-        <Drawer.Positioner >
-          <Drawer.Content  borderRadius={"10px"} boxShadow={"dark-lg"}>
+        <Drawer.Positioner>
+          <Drawer.Content borderRadius={"10px"} boxShadow={"dark-lg"}>
             <Drawer.Header>
-              <Drawer.Title>Frames</Drawer.Title>
+              <Text>Frames</Text>
             </Drawer.Header>
             <Drawer.Body>
-                
-                <FrameCard />
-
-
+              <SegmentGroup.Root defaultValue="Frames">
+                <SegmentGroup.Indicator />
+                <SegmentGroup.Items items={["Frames", "Options"]} />
+              </SegmentGroup.Root>
+              <FrameCard />
             </Drawer.Body>
             <Drawer.Footer>
-              <Button variant="outline" onClick={() => setIsOpen(false)}>Cancel</Button>
+              <Button variant="outline" onClick={() => setIsOpen(false)}>
+                Cancel
+              </Button>
               <Button>Save</Button>
             </Drawer.Footer>
             <Drawer.CloseTrigger asChild>
@@ -32,5 +42,5 @@ export const FramesDrawer = () => {
         </Drawer.Positioner>
       </Portal>
     </Drawer.Root>
-  )
-}
+  );
+};
--- src/components/ImageLoader/ImageLoader.tsx
+++ src/components/ImageLoader/ImageLoader.tsx
@@ -6,11 +6,9 @@ interface ImageLoaderProps {
 
 }
 
-
-
 export const ImageLoader:React.FC<ImageLoaderProps> = ({imageUrl}) => {
   return (
-    <Box  h={'full'} p={10} boxSizing={'content-box'} bg={'linear-gradient(90deg,rgba(155, 42, 149, 1) 0%, rgba(250, 22, 22, 1) 71%)'} borderRadius={'10px'} overflow={'hidden'} >
+    <Box  h={'full'} p={10} boxSizing={'content-box'}overflow={'hidden'} >
         <Box  h={'full'} border={'2px solid #292929'} borderRadius={'10px'} overflow={'hidden'} >
             <Flex className='navbar' gap={2}>
             <Circle size={4} bgColor={'#F54927'}/>
@@ -18,7 +16,6 @@ export const ImageLoader:React.FC<ImageLoaderProps> = ({imageUrl}) => {
             <Circle size={4} bgColor={'#99E340'}/>
             </Flex>
             <Image  src={imageUrl} objectFit={"contain"} alt={"table frame"}  h={"100%"}/>
-
         </Box>
     </Box>
 
